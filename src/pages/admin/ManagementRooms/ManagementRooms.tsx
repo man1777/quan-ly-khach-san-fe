c9bc10a05e2a15ffcee10091475a3c6a6a974abf
man1777 <man1772003@gmail.com>
2025-03-18 16:54:06 +0700

    add modal component for saving room details with form validation and API integration

--- src/pages/admin/ManagementRooms/ManagementRooms.tsx
+++ src/pages/admin/ManagementRooms/ManagementRooms.tsx
@@ -1,135 +1,220 @@
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
-import { Card, Select, Tag } from "antd";
-import "../../../styles/adminStylesPage/ManagementRoomsStyle.css"
-import { formatCurrency } from "../../../utils/utils"
+import { EditOutlined, SettingOutlined } from "@ant-design/icons";
+import { Button, Card, Result, Select, Skeleton } from "antd";
+import "../../../styles/adminStylesPage/ManagementRoomsStyle.css";
+// import { formatCurrency } from "../../../utils/utils"
 import Search, { SearchProps } from "antd/es/input/Search";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Option } from "antd/es/mentions";
+import ModalSaveRoom from "./ModalSaveRoom";
 const ManagementRooms = () => {
-  const dataRooms: Room[] = [];
+  const [dataRoomsFilter, setDataRoomsFilter] = useState<RoomData[]>([]);
 
-  let [dataRoomsFilter] = useState<Room[]>();
+  const [searchInput, setSearchInput] = useState("");
 
-  const [searchInput, setSearchInput] = useState('')
+  // const [optionsTinhTrang] = useState<string[]>(["Ready", "Booked"]);
 
-  const [optionsTinhTrang, setOptionsTinhTrang] = useState<string[]>(["Ready", "Booked"])
+  const [statusRoom, setStatusRoom] = useState<string>("All");
 
-  let [statusRoom, setStatusRoom] = useState<string>('')
+  const [floorRoom, setFloorRoom] = useState<number>(0);
 
-  let [dataRoom, setDataRoom] = useState<RoomData[]>([])
+  const [dataRoom, setDataRoom] = useState<RoomData[]>([]);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [isShowModal, setIsShowModal] = useState<boolean>(false);
+
+  const [dataRoomType, setDataRoomType] = useState<RoomType[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-
-      const params = {
-        PageNumber: 1,
-        PageSize: 50,
-        Depth: 0,
-        Search: ''
-      };
-      axios.get(`https://hotelmanagementapi20250217124648.azurewebsites.net/api/Room?${params.PageNumber}&${params.PageSize}&${params.Depth}`).then(res => {
-        if (res) {
-          setDataRoom(res.data.items)
-        }
-      })
-    };
-    fetchData();
+    getDataRooms();
+    loadRoomType();
   }, []);
 
-  const optionsLoaiPhong =
-    [
-      {
-        label: "Phòng đơn",
-        value: 1
-      },
-      {
-        label: "Phòng đôi",
-        value: 2
-      },
-      {
-        label: "Phòng ba",
-        value: 3
-      },
-      {
-        label: "Phòng bốn",
-        value: 4
-      },
-    ];
+  const getDataRooms = async () => {
+    setIsLoading(true);
+    const params = {
+      PageNumber: 1,
+      PageSize: 50,
+      Depth: 0,
+      Search: "",
+    };
+    axios
+      .get(
+        `https://hotelmanagementapi20250217124648.azurewebsites.net/api/Room?${params.PageNumber}&${params.PageSize}&${params.Depth}`
+      )
+      .then((res) => {
+        if (res) {
+          setDataRoom(res.data.items);
+          setDataRoomsFilter(res.data.items);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-  const optionsGiaTien =
-    [
-      {
-        label: "0 - 1.000.000 VNĐ",
-        value: 1
-      },
-      {
-        label: "1.000.000 - 3.000.000 VNĐ",
-        value: 2
-      },
-      {
-        label: "3.000.000 - 10.000.000 VNĐ",
-        value: 3
-      },
-      {
-        label: "10.000.000 - 50.000.000 VNĐ",
-        value: 4
-      },
-    ];
+  const optionsLoaiPhong = [
+    {
+      label: "All floors",
+      value: 0,
+    },
+    {
+      label: "1 floor",
+      value: 1,
+    },
+    {
+      label: "2 floors",
+      value: 2,
+    },
+    {
+      label: "3 floors",
+      value: 3,
+    },
+  ];
 
+  // const optionsGiaTien = [
+  //   {
+  //     label: "0 - 1.000.000 VNĐ",
+  //     value: 1,
+  //   },
+  //   {
+  //     label: "1.000.000 - 3.000.000 VNĐ",
+  //     value: 2,
+  //   },
+  //   {
+  //     label: "3.000.000 - 10.000.000 VNĐ",
+  //     value: 3,
+  //   },
+  //   {
+  //     label: "10.000.000 - 50.000.000 VNĐ",
+  //     value: 4,
+  //   },
+  // ];
 
-  const fakeDataRoom = () => {
-    for (let i = 0; i < 20; i++) {
-      const data = {
-        room_id: i,
-        room_number: "P10" + i,
-        room_type: "classic",
-        price: Number(i + '000000'),
-        status: 1,
-        description: "phòng đẹp",
-        bed_count: 2
-      }
-      dataRooms.push(data)
-    }
-  }
-
-  const renderStatus = (room: Room) => {
-    if (room.status === 1) return <Tag color="green" style={{ marginInlineEnd: '0px' }}>Còn trống</Tag>;
-    if (room.status === 2) return <Tag color="gold">Đã đặt phòng</Tag>;
-    if (room.status === 3) return <Tag color="magenta">Đã đặt trước</Tag>;
-    return <Tag color="red">Đang bảo trì</Tag>;
-  }
+  // const renderStatus = (room: Room) => {
+  //   if (room.status === 1) return <Tag color="green" style={{ marginInlineEnd: '0px' }}>Còn trống</Tag>;
+  //   if (room.status === 2) return <Tag color="gold">Đã đặt phòng</Tag>;
+  //   if (room.status === 3) return <Tag color="magenta">Đã đặt trước</Tag>;
+  //   return <Tag color="red">Đang bảo trì</Tag>;
+  // }
 
   const classCenter = () => {
+    const className = "flex justify-between items-center";
+    return className;
+  };
 
-    const className = "flex justify-between items-center"
-    return className
-  }
+  // const classStatusRoom = (status: string) => {
+  //   let className = "";
+  //   if (status == "Ready") {
+  //     className = "bg-green-300";
+  //   } else if (status == "Booked") {
+  //     className = "bg-red-400";
+  //   }
+  //   return className;
+  // };
 
-  const classStatusRoom = (status: string) => {
-    let className = ""
-    if (status == "Ready") {
-      className = "bg-green-300"
-    } else if (status == "Booked") {
-      className = "bg-red-400"
+  const onSearch: SearchProps["onSearch"] = (value) => {
+    setDataRoom(
+      dataRoomsFilter.filter((item) => item.roomNumber.includes(value))
+    );
+  };
+
+  const onHandleChangeStatus = (value: string) => {
+    setStatusRoom(value);
+  };
+
+  const onHandleChangeFloor = (value: number) => {
+    setFloorRoom(value);
+  };
+
+  const renderRoom = () => {
+    if (isLoading) {
+      return <Skeleton active />;
+    } else {
+      const data = dataRoom
+        .filter((room) => {
+          if (statusRoom === "All") return true;
+          else return room.status === statusRoom;
+        })
+        .filter((room) => {
+          if (floorRoom === 0) return true;
+          else return room.floor === floorRoom;
+        });
+      if (data.length === 0) {
+        return (
+          <div className="text-center">
+            <Result title="Không có phòng nào" />
+          </div>
+        );
+      } else {
+        return (
+          <div className="grid grid-cols-5 gap-5 ">
+            {data.map((room) => {
+              return (
+                <div key={room.id}>
+                  <Card
+                    className={`w-full hover:shadow-2xl duration-300 hover:scale-102`}
+                    cover={
+                      <img
+                        alt="example"
+                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                      />
+                    }
+                    style={{
+                      backgroundColor: `${
+                        room.status == "Ready" ? "#32cd32" : "#c7090969"
+                      }`,
+                    }}
+                    actions={[
+                      <EditOutlined key="edit" />,
+                      <SettingOutlined key="setting" />,
+                    ]}
+                  >
+                    <div className={` "flex flex-col"`}>
+                      <div className={classCenter()}>
+                        <b>Số phòng: </b> <span>{room.roomNumber}</span>
+                      </div>
+                      <div className={classCenter()}>
+                        <b>Tầng: </b> <span>{room.floor}</span>
+                      </div>
+                      <div className={classCenter()}>
+                        <b>Trạng thái: </b> <span>{room.status}</span>
+                      </div>
+                      {/* <div className={classCenter()}>
+                      <b>Giá phòng: </b> <span>{formatCurrency(room.price)}</span>
+                    </div>
+                    <div className={classCenter()}>
+                      <b>Tình trạng: </b> {renderStatus(room)}
+                    </div>
+                    <div>{room.description}</div> */}
+                    </div>
+                  </Card>
+                </div>
+              );
+            })}
+          </div>
+        );
+      }
     }
-    return className
-  }
+  };
 
-  const handleChange = () => {
-
-  }
-
-  const onSearch: SearchProps['onSearch'] = (value) => {
-    dataRoomsFilter = dataRooms.filter(item => item.room_number.includes(value))
-    console.log('dataRoomsFilter', dataRoomsFilter);
-  }
-
-  fakeDataRoom()
-
-  const onHandleChangeStatus = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setStatusRoom(e.target.value)
-  }
+  const loadRoomType = () => {
+    const params = {
+      PageNumber: 1,
+      PageSize: 50,
+      Depth: 0,
+      Search: "",
+    };
+    axios
+      .get(
+        `https://hotelmanagementapi20250217124648.azurewebsites.net/api/RoomType?${params.PageNumber}&${params.PageSize}&${params.Depth}`
+      )
+      .then((res) => {
+        if (res) {
+          setDataRoomType(res.data.items);
+        }
+      });
+  };
 
   return (
     <>
@@ -137,107 +222,93 @@ const ManagementRooms = () => {
         <h1>ManagementRooms</h1>
       </div>
       {/* Thanh filter */}
-      <div className="my-5 flex gap-4 justify-between">
-
+      <div className="my-5 flex gap-4 justify-between ring-2 ring-blue-500/50 p-5 rounded-md mb-10">
         <div className="flex gap-4">
           <Select
+            value={floorRoom}
             size="large"
-            placeholder="Loại phòng"
+            placeholder="Chọn tầng"
             style={{ width: 200 }}
             options={optionsLoaiPhong}
+            onChange={onHandleChangeFloor}
           />
           <Select
             size="large"
+            value={statusRoom}
             placeholder="Tình trạng phòng"
             onChange={onHandleChangeStatus}
             style={{ width: 200 }}
-          >
-
-            {optionsTinhTrang.map((option) => (
-              <Option  value={option}>
-                {option}
-              </Option>
-            ))}
-          </Select>
-          <Select
+            options={[
+              { value: "All", label: <span>All rooms</span> },
+              { value: "Ready", label: <span>Ready</span> },
+              { value: "Booked", label: <span>Booked</span> },
+            ]}
+          ></Select>
+          {/* <Select
             size="large"
             placeholder="Giá phòng"
             onChange={handleChange}
             style={{ width: 250 }}
             options={optionsGiaTien}
-          />
+          /> */}
         </div>
-        <div className="w-100">
-          <Search value={searchInput} placeholder="Tìm kiếm theo số phòng" onSearch={onSearch} onChange={(e) => setSearchInput(e.target.value)} enterButton size="large" />
+        <div className="w-100 flex gap-4">
+          <Search
+            value={searchInput}
+            placeholder="Tìm kiếm theo số phòng"
+            onSearch={onSearch}
+            onChange={(e) => setSearchInput(e.target.value)}
+            enterButton
+            size="large"
+          />
+          <Button
+            size="large"
+            type="primary"
+            onClick={() => setIsShowModal(true)}
+          >
+            Thêm mới
+          </Button>
         </div>
       </div>
       {/* Render các phòng */}
-      <div className="grid grid-cols-5 gap-5 ">
-        {
-          dataRoom.map((room) => {
-            return (
-              <div key={room.id} className={classStatusRoom(room.status)}>
-                <Card
-                  className="w-full hover:shadow-2xl duration-300 hover:scale-102"
-                  cover={
-                    <img
-                      alt="example"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                    />
-                  }
-
-                  actions={[
-                    <SettingOutlined key="setting" />,
-                    <EditOutlined key="edit" />,
-                    <EllipsisOutlined key="ellipsis" />,
-                  ]}
-                >
-                  <div className="flex flex-col">
-                    <div className={classCenter()}>
-                      <b>Số phòng: </b> <span>{room.roomNumber}</span>
-                    </div>
-                    <div className={classCenter()}>
-                      <b>Số cửa sổ: </b> <span>{room.floor}</span>
-                    </div>
-                    <div className={classCenter()}>
-                      <b>Trạng thái: </b> <span>{room.status}</span>
-                    </div>
-                    {/* <div className={classCenter()}>
-                  <b>Giá phòng: </b> <span>{formatCurrency(room.price)}</span>
-                </div>
-                <div className={classCenter()}>
-                  <b>Tình trạng: </b> {renderStatus(room)}
-                </div>
-                <div>{room.description}</div> */}
-                  </div>
-
-                </Card>
-              </div>
-            );
-          })}</div>
+      {renderRoom()}
+      <ModalSaveRoom
+        isShowModal={isShowModal}
+        setIsShowModal={setIsShowModal}
+        dataRoomType={dataRoomType}
+        onSuccessful={() => getDataRooms()}
+      ></ModalSaveRoom>
     </>
   );
 };
 
-interface Room {
-  room_id: number;
-  room_number: string;
-  room_type: string;
-  price: number;
-  status: number;
-  description: string;
-  bed_count: number
+interface RoomData {
+  roomNumber: string;
+  thumbnail: string;
+  floor: number;
+  status: string;
+  bookings: Array<string>;
+  images: Array<string>;
+  id: number;
+  createdAt: string;
+  updatedAt: string;
 }
 
-interface RoomData {
-  roomNumber: string,
-  thumbnail: string,
-  floor: number
-  status: string,
-  bookings: Array<string>,
-  images: Array<string>,
-  id: number
-  createdAt: string,
-  updatedAt: string,
+interface RoomType {
+  name: string;
+  description: string;
+  pricePerNight: number;
+  numberOfBathrooms: number;
+  numberOfBeds: number;
+  singleBed: number;
+  doubleBed: number;
+  capacity: number;
+  sizes: number;
+  thumbnail: string;
+  Images: Array<string>;
+  amenities: Array<string>;
+  id: number;
+  createdAt: string;
+  updatedAt: string;
 }
 export default ManagementRooms;
---
+++ src/pages/admin/ManagementRooms/ModalSaveRoom.tsx
@@ -0,0 +1,139 @@
+import { Form, Input, message, Modal, Select } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface ModalSaveRoomProps {
+  isShowModal: boolean;
+  setIsShowModal: (value: boolean) => void;
+  dataRoomType: RoomType[];
+  onSuccessful: () => void;
+}
+
+interface RoomType {
+  name: string;
+  description: string;
+  pricePerNight: number;
+  numberOfBathrooms: number;
+  numberOfBeds: number;
+  singleBed: number;
+  doubleBed: number;
+  capacity: number;
+  sizes: number;
+  thumbnail: string;
+  Images: Array<string>;
+  amenities: Array<string>;
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const ModalSaveRoom = ({
+  isShowModal,
+  setIsShowModal,
+  dataRoomType,
+  onSuccessful,
+}: ModalSaveRoomProps) => {
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const [form] = Form.useForm();
+
+  useEffect(() => {}, [isShowModal]);
+  const handleOk = () => {
+    setConfirmLoading(true);
+    const formData = form.getFieldsValue();
+    const dataInsert = new FormData();
+    dataInsert.append("HotelId", "0");
+    dataInsert.append("RoomTypeId", formData.RoomTypeId);
+    dataInsert.append("RoomNumber", formData.RoomNumber);
+    dataInsert.append("Floor", formData.Floor);
+    dataInsert.append("Status", formData.Status);
+    dataInsert.append("Thumbnail", "");
+    dataInsert.append("Images", "");
+    axios
+      .post(
+        "https://hotelmanagementapi20250217124648.azurewebsites.net/api/Room",
+        dataInsert
+      )
+      .then((res) => {
+        if (res) {
+          form.resetFields();
+          setIsShowModal(false);
+          onSuccessful();
+          message.success("Lưu phòng thành công!");
+        }
+      })
+      .finally(() => {
+        setConfirmLoading(false);
+      });
+  };
+
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setIsShowModal(false);
+  };
+  return (
+    <>
+      <Modal
+        title="Thêm mới phòng"
+        open={isShowModal}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <Form name="formData" onFinish={handleOk} form={form}>
+          <Form.Item<ModalProps>
+            label="Room type"
+            name="RoomTypeId"
+            rules={[{ required: true, message: "Please choose room type!" }]}
+          >
+            <Select
+              labelRender={(option) => option.label}
+              placeholder="Select room type"
+              options={dataRoomType.map((item) => {
+                return { label: item.name, value: item.id };
+              })}
+            ></Select>
+          </Form.Item>
+          <Form.Item<ModalProps>
+            label="Room Number"
+            name="RoomNumber"
+            rules={[{ required: true, message: "Please input Room Number!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<ModalProps>
+            label="Floor"
+            name="Floor"
+            rules={[{ required: true, message: "Please input Floor!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<ModalProps>
+            label="Status"
+            name="Status"
+            rules={[{ required: true, message: "Please choose Status!" }]}
+          >
+            <Select
+              options={[
+                { label: "Ready", value: "Ready" },
+                { label: "Booked", value: "Booked" },
+              ]}
+            ></Select>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+};
+
+interface ModalProps {
+  HotelId: number;
+  RoomTypeId: number;
+  RoomNumber: string;
+  Floor: number;
+  Status: string;
+  Thumbnail: string;
+  Images: [];
+}
+export default ModalSaveRoom;
